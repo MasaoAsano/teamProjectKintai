f0e36566a3a6c00d6337a2a5947cb31deed1f053
ejiri <takeshi_ejiri@mail.toyota.co.jp>
2022-11-24 00:49:43 +0900

    fix(WorkstyleSelector)!: 選択が消える不具合修正

--- kintai/src/Components/WorkstyleSelector.js
+++ kintai/src/Components/WorkstyleSelector.js
@@ -1,9 +1,10 @@
 const WorkstyleSelector = ({ selected }) => {
+  const uniqueName = "workstyle" + Math.random()
   return (
     <div  className="workstyleSelector">
       {
         ["在社", "在宅"].map( style => <>
-          <input key={style} type="radio" id={style} name="workstyle" value={style} defaultChecked={style===selected} />
+          <input key={style} type="radio" id={style} name={uniqueName} value={style} defaultChecked={style===selected} />
           <label key={style + "label"} htmlFor={style}>{style}</label>
         </>)
       }
