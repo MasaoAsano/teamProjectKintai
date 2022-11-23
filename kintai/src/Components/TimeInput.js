9e84f9f6cb3bd1ea869f8e811f20221fe7050c85
ejiri <takeshi_ejiri@mail.toyota.co.jp>
2022-11-23 11:19:20 +0900

    feat(TimeInput)!: 時刻選択結果を入力欄に反映できるようにした

--- kintai/src/Components/TimeInput.js
+++ kintai/src/Components/TimeInput.js
@@ -1,11 +1,40 @@
+import { useEffect, useState } from "react";
 import TimeSelectionTable from "./TimeSelectionTable";
 
 const TimeInput = () => {
+  const dummyDate = new Date()
+  const createDisplayTime = (date) => date.getHours() + ":" + String(date.getMinutes()).padStart(2, "0");
+  const [startTime, setStartTime] = useState(dummyDate);
+  const [displayTimeStart, setDisplayTimeStart] = useState(createDisplayTime(startTime));
+  const [endTime, setEndTime] = useState(dummyDate);
+  const [displayTimeEnd, setDisplayTimeEnd] = useState(createDisplayTime(endTime));
+
+  useEffect(() => {
+    setDisplayTimeStart(createDisplayTime(startTime));
+  }, [startTime])
+  useEffect(() => {
+    setDisplayTimeEnd(createDisplayTime(endTime));
+  }, [endTime])
+
+  const timeInputted = (e) => {
+    if (e.target.placeholder === "開始時刻") {
+      setDisplayTimeStart(e.target.value);
+    } else {
+      setDisplayTimeEnd(e.target.value);
+    }
+  }
+
+  const timeInputElement = (displayTime, placeholder) => (
+    <input type="text" placeholder={placeholder} value={displayTime} onChange={timeInputted} />
+  );
+
   return (
     <div className="timeInput">
-      <input type="text" /><TimeSelectionTable defaultDate={new Date}/>
+      {timeInputElement(displayTimeStart, "開始時刻")}
+      <TimeSelectionTable defaultDate={startTime} setTime={setStartTime}/>
       <p>~</p>
-      <input type="text" />
+      {timeInputElement(displayTimeEnd, "終了時刻")}
+      <TimeSelectionTable defaultDate={endTime} setTime={setEndTime}/>
     </div>
   )
 };
