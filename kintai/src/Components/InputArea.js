ea177d10da1c1e078d78a721f1bf739272e61f82
kazu006 <kazuhiro_higuchi@mail.toyota.co.jp>
2022-11-23 22:57:54 +0900

    templete選択時の処理

--- kintai/src/Components/InputArea.js
+++ kintai/src/Components/InputArea.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Memo from "./Memo";
 import Register from "./Register";
 import Template from "./Template";
@@ -8,13 +8,25 @@ import Title from "./Title";
 
 const InputArea = (props) => {
   const [date, setDate] = useState(new Date());
-  const [currentTemplate, setCurrentTemplate] = useState("昨日と同じ");
+  const [currentTemplate, setCurrentTemplate] = useState("A");
   const [memo, setMemo] = useState("メモ");
+  let templateList = [
+    { id: "A", displayName: "在宅" ,timeList:[{startHour:8, startMinute:0, endHour:17, endMinute:0, style:"在宅"}]},
+    { id: "B", displayName: "在社" ,timeList:[{startHour:8, startMinute:30, endHour:17, endMinute:30, style:"在社"}]},
+    { id: "C", displayName: "在社→在宅" ,timeList:[{startHour:8, startMinute:0, endHour:12, endMinute:0, style:"在社"},{startHour:13, startMinute:0, endHour:17, endMinute:0, style:"在宅"}]},
+    { id: "D", displayName: "出張" ,timeList:[{startHour:9, startMinute:0, endHour:18, endMinute:0, style:"在社"}]},
+  ];
+  const [currentTimeList, setCurrentTimeList] = useState(templateList[0].timeList);
+  useEffect(() => {
+    const template = templateList.find(el => el.id === currentTemplate);
+    setCurrentTimeList(template.timeList);
+  },[currentTemplate])
+  
   return (
     <div className="inputArea">
       <Title date={date} />
-      <Template currentTemplate={currentTemplate} setCurrentTemplate={setCurrentTemplate} />
-      <TimeField />
+      <Template currentTemplate={currentTemplate} setCurrentTemplate={setCurrentTemplate} templateList={templateList}/>
+      <TimeField currentTimeList={currentTimeList}/>
       <Memo memo={memo} func={setMemo} />
       <TimeComment />
       <Register />
--- kintai/src/Components/Template.js
+++ kintai/src/Components/Template.js
@@ -1,24 +1,19 @@
-import { useState } from "react";
-
 // TODO:
 // const templates = fetch(この人のテンプレート一覧を取得する);
 
-const Template = ({ currentTemplate, setCurrentTemplate }) => {
+const Template = ({ currentTemplate, setCurrentTemplate, templateList }) => {
   // const [templateList, setTemplateList] = useState([currentTemplate]);
 
-  let templateList = [
-    { id: "A", displayName: "在宅" },
-    { id: "B", displayName: "出社" },
-    { id: "C", displayName: "出社＆在宅" },
-    { id: "D", displayName: "出張" },
-  ];
 
 
+  
   return (
     <div className="">
     <select onChange={(e) => {
       let city = e.target.value;
+      console.log('city',city);
       setCurrentTemplate(city ? city : null);
+      console.log('currentTemplate',currentTemplate);
     }}>
       <option value="">（勤務テンプレート選択…）</option>
       {templateList.map(cityOption => <option key={cityOption.id} value={cityOption.id}>{cityOption.displayName}</option>)}
