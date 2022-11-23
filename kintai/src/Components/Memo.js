5640571f5a5e45518280d7da39559bb15328bf00
kazu006 <kazuhiro_higuchi@mail.toyota.co.jp>
2022-11-23 19:57:14 +0900

    first

--- kintai/src/Components/Memo.js
+++ kintai/src/Components/Memo.js
@@ -1,6 +1,15 @@
-const Memo = () => {
+const Memo = ({ memo,func }) => {
   return (
-    <div></div>
+    <div>
+      <p>メモ欄</p>
+      <input
+      type="text"
+      onChange={(e) => {
+        func=(e.target.value)
+      }}
+      />
+      <p>{memo}</p>
+    </div>
   )
 };
 
